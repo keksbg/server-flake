aaa260a5d8da80a4fbc4b72001bb00927670d8f2
keksbg <3230579-keksbg@users.noreply.gitlab.com>
2022-06-21 18:43:33 +0200

    empty password for diaphanus

--- users/stateless/default.nix
+++ users/stateless/default.nix
@@ -13,6 +13,7 @@
 
   users.users.diaphanus = {
     isNormalUser = true;
+    hashedPassword = "";
     extraGroups = [ "wheel" ];
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMOCOtko0hLYeIAW79Gv6w2xWkUkiMtnhWtJU9O9kpPS arch" ];
